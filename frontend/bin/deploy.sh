f01c848cb76054b587de38764d3c68a523931001
Anthony Wittig <anthony.wittig@gmail.com>
2020-11-21 15:27:24 -0700

    clear cloudfront cache

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -10,3 +10,6 @@ cd ../
 npm run build
 
 aws --profile=mLock s3 cp build s3://mlock-site --recursive
+
+# clear the cache
+aws --profile=mLock cloudfront create-invalidation --distribution-id E3RMAC8N7J8VMP --paths "/*"
