79b49084389096c21b7e8973b98ec1ae952334d0
Anthony Wittig <anthony.wittig@gmail.com>
2020-12-28 13:59:45 -0700

    some signin improvements

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,55 +4,83 @@ import {
     GoogleLoginResponse,
     GoogleLoginResponseOffline, 
 } from 'react-google-login';
+//import { useHistory } from 'react-router-dom';
 
-const responseGoogleSuccess = (response: GoogleLoginResponse | GoogleLoginResponseOffline) => {
 
+type Props = {};
 
-    if ((response as GoogleLoginResponse).profileObj) {
-        const user = response as GoogleLoginResponse;
-        const gToken = user.getAuthResponse().id_token;
+type State = {
+    error: string;
+};
 
-        fetch("https://api2.zcclock.com/sign-in", {
-            method: "POST",
-            body: JSON.stringify({googleToken: gToken})
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log(response);
-            /*
-            this.setState({
-                users: response.Users,
-                newUser: "",
-                newUserFieldEnabled: true,
-            });
-            */
-        })
-        .catch(err => {
-            // Need to indicate error...
-            console.log(err);
-        });
+const ERROR_NOT_AUTHORIZED = 'not authorized';
+const ERROR_UNKNOWN = 'unknown';
 
-    } else {
-        // consider failure?
-        //this.failureResponse(response)
+export class SignIn extends React.Component<Props, State> {
+    state: Readonly<State> = {
+        error: "",
     }
-}
 
-const responseGoogleFailure = (response: any) => {
-    console.log(response);
-}
+    responseGoogleSuccess(response: GoogleLoginResponse | GoogleLoginResponseOffline) {
+        if ((response as GoogleLoginResponse).profileObj) {
+            const user = response as GoogleLoginResponse;
+            const gToken = user.getAuthResponse().id_token;
 
-export const SignIn = () => {
-    return (<div>
-        <h2>Sign In</h2>
-        <br />
-        <GoogleLogin
-            clientId={process.env.REACT_APP_GOOGLE_SIGNIN_CLIENT_ID || ""}
-            buttonText="Login"
-            onSuccess={responseGoogleSuccess}
-            onFailure={responseGoogleFailure}
-            cookiePolicy={'single_host_origin'}
-        />
-        <div className="g-signin2" data-onsuccess="onSignIn"></div>
-    </div>);
+            fetch("https://api2.zcclock.com/sign-in", {
+                method: "POST",
+                body: JSON.stringify({googleToken: gToken})
+            })
+            .then(response => {
+                switch(response.status) {
+                    case 403:
+                        this.setState({ error: ERROR_NOT_AUTHORIZED });
+                        return
+                }
+
+                // this is a promise: console.log(response.json());
+                // grab token from response and redirect to somewhere useful?
+            }).catch(err => {
+                console.log(err);
+                this.setState({ error: ERROR_UNKNOWN });
+            });
+        } else {
+            // consider failure?
+            //this.failureResponse(response)
+        }
+    }
+
+    responseGoogleFailure(response: any) {
+        console.log(response);
+    }
+
+    render() {
+        let errorMessage = ""
+        switch(this.state.error) {
+            case "":
+                // Do nothing.
+                break;
+            case ERROR_NOT_AUTHORIZED:
+                errorMessage = "Not Authorized - request access from an administrator"
+                break;
+            default: // Includes `ERROR_UNKNOWN`.
+                errorMessage = "An error has occurred"
+        }
+        return (<div>
+            <h2>Sign In</h2>
+            <br />
+            {
+                errorMessage &&
+                <div className="alert alert-danger" role="alert">{errorMessage}</div>
+            }
+            <br />
+            <GoogleLogin
+                clientId={process.env.REACT_APP_GOOGLE_SIGNIN_CLIENT_ID || ""}
+                buttonText="Login"
+                onSuccess={this.responseGoogleSuccess.bind(this)}
+                onFailure={this.responseGoogleFailure.bind(this)}
+                cookiePolicy={'single_host_origin'}
+            />
+            <div className="g-signin2" data-onsuccess="onSignIn"></div>
+        </div>);
+    }
 }
