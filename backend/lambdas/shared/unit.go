52a0fbe5021429c65c94694bd903b8239500eba3
Anthony Wittig <anthonywittig@anthonys-mbp-2.lan>
2024-03-27 20:16:40 -0600

    Displaying occupancy status on climate control DP.

--- backend/lambdas/shared/unit.go
+++ backend/lambdas/shared/unit.go
@@ -17,6 +17,18 @@ type Unit struct {
 	UpdatedBy         string    `json:"updatedBy"`
 }
 
+type UnitOccupancyStatus struct {
+	Date time.Time `json:"date"`
+	Noon struct {
+		Occupied         bool                    `json:"occupied"`
+		ManagedLockCodes []DeviceManagedLockCode `json:"managedLockCodes"`
+	} `json:"noon"`
+	FourPM struct {
+		Occupied         bool                    `json:"occupied"`
+		ManagedLockCodes []DeviceManagedLockCode `json:"managedLockCodes"`
+	} `json:"fourPm"`
+}
+
 func (u *Unit) GetRemotePropertyID() int {
 	// RemotePropertyURL is of the form:
 	// https://dashboard.hostaway.com/listing/211374
@@ -30,4 +42,37 @@ func (u *Unit) GetRemotePropertyID() int {
 		return -1
 	}
 	return intID
+}
+
+func (u *Unit) OccupancyStatusForDay(devices []Device, day time.Time) (UnitOccupancyStatus, error) {
+	if day.Hour() != 0 || day.Minute() != 0 || day.Second() != 0 || day.Nanosecond() != 0 {
+		return UnitOccupancyStatus{}, fmt.Errorf("day must have zero time fields")
+	}
+
+	noon := day.Add(12 * time.Hour)
+	fourPM := day.Add(16 * time.Hour)
+
+	unitOccupiedStatus := UnitOccupancyStatus{
+		Date: day,
+	}
+
+	for _, d := range devices {
+		if d.UnitID != nil && *d.UnitID == u.ID {
+			for _, mlc := range d.ManagedLockCodes {
+				if mlc.Reservation.ID != "" {
+					// Warning: we're basing these off of the lock start/stop times which should have a buffer around when we expect the unit to be occupied. If we adjust that buffer we might break this logic.
+					if (mlc.StartAt.Before(noon) || mlc.StartAt == noon) && mlc.EndAt.After(noon) {
+						unitOccupiedStatus.Noon.Occupied = true
+						unitOccupiedStatus.Noon.ManagedLockCodes = append(unitOccupiedStatus.Noon.ManagedLockCodes, *mlc)
+					}
+					if (mlc.StartAt.Before(fourPM) || mlc.StartAt == fourPM) && mlc.EndAt.After(fourPM) {
+						unitOccupiedStatus.FourPM.Occupied = true
+						unitOccupiedStatus.FourPM.ManagedLockCodes = append(unitOccupiedStatus.FourPM.ManagedLockCodes, *mlc)
+					}
+				}
+			}
+		}
+	}
+
+	return unitOccupiedStatus, nil
 }
